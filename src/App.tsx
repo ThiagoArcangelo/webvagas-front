b3bf1bd82aaf1789d1a1c8a1f5c54a74acd43d9e
Thiago da Silva Arcangelo <thiago.arcangelo@confianca.com.br>
2024-07-15 12:54:20 -0300

    style: Ajuste no posicionamento do footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import { Footer } from "./components/footer";
 function App() {
 
   return (
-      <div className="h-100%">
+      <div className="h-100% flex flex-col h-screen justify-between">
         <Header />
           <div className=" w-[70%] h-[75vh] flex ml-[15%] mr-30 border-none mt-0">
             <Tabela />
           </div>
-          <div className="mb-1-8 w-=[10%]">
+          <div className=" h-12 w-=[7%]">
             <Footer />
           </div>
       </div>
