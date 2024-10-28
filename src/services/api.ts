fd72a0d71215369b1b5c1d2f640bb8583cc1283d
Thiago da Silva Arcangelo <thiago.arcangelo@confianca.com.br>
2024-10-28 12:36:59 -0300

    feat: Implementa as dependências do analytics da Vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3333",
-  baseURL: "https://listaempregosapi.netlify.app",
+  baseURL: process.env.REACT_APP_URL_API,
 });
 
 export default api;
