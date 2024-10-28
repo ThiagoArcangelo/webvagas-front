0f2179caf44187c3219bc8df2c212507e21035ce
ThiagoArcangelo <devtsarcangelo@gmail.com>
2024-10-27 23:39:36 -0300

    fix: Ajusta a url da api com o Cor configurado

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3333",
-  baseURL: "https://listaempregosapi.vercel.app/lista",
+  baseURL: "https://listaempregosapi.vercel.app",
 });
 
 export default api;
